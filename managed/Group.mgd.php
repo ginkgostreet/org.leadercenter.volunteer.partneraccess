f850b4c9d321ce0b8aa716ccd48f27ad0651569e
Frank J. Gómez <frank@ginkgostreet.com>
2017-03-04 11:31:25 -0500

    VARL-226: Updated descriptions of group types to account for change in approach.

--- managed/Group.mgd.php
+++ managed/Group.mgd.php
@@ -19,7 +19,7 @@ return array(
     'name' => 'Volunteer Arlington Partner Access - Optin Group Type',
     'entity' => 'OptionValue',
     'params' => array(
-      'description' => ts('Groups of this type represent contacts who have manually opted in to receive communications from the group creator.', array('org.leadercenter.volunteer.partneraccess')),
+      'description' => ts('Groups of this type represent contacts who have manually opted in to receive communications from the partner specified via custom field.', array('org.leadercenter.volunteer.partneraccess')),
       'is_reserved' => 1,
       'label' => 'Volunteer Arlington Partner Access - Optin',
       'name' => 'varl_partner_access_optin',
@@ -33,7 +33,7 @@ return array(
     'name' => 'Volunteer Arlington Partner Access - Optout Group Type',
     'entity' => 'OptionValue',
     'params' => array(
-      'description' => ts('Groups of this type represent contacts who have manually opted out of receiving communications from the group creator. For the purposes of access control, membership in this group trumps membership in any other.', array('org.leadercenter.volunteer.partneraccess')),
+      'description' => ts('Groups of this type represent contacts who have manually opted out of receiving communications from the partner specified via custom field. For the purposes of access control, membership in this group trumps membership in any other.', array('org.leadercenter.volunteer.partneraccess')),
       'is_reserved' => 1,
       'label' => 'Volunteer Arlington Partner Access - Optout',
       'name' => 'varl_partner_access_optout',
@@ -47,7 +47,7 @@ return array(
     'name' => 'Volunteer Arlington Partner Access - Staff Group Type',
     'entity' => 'OptionValue',
     'params' => array(
-      'description' => ts('Groups of this type represent contacts who are active staff of the group creator.', array('org.leadercenter.volunteer.partneraccess')),
+      'description' => ts('Groups of this type represent contacts who are active staff of the partner specified via custom field.', array('org.leadercenter.volunteer.partneraccess')),
       'is_reserved' => 1,
       'label' => 'Volunteer Arlington Partner Access - Staff',
       'name' => 'varl_partner_access_staff',
@@ -61,7 +61,7 @@ return array(
     'name' => 'Volunteer Arlington Partner Access - Volunteer Group Type',
     'entity' => 'OptionValue',
     'params' => array(
-      'description' => ts('Groups of this type represent contacts who have volunteered with the group creator.', array('org.leadercenter.volunteer.partneraccess')),
+      'description' => ts('Groups of this type represent contacts who have volunteered with the partner specified via custom field.', array('org.leadercenter.volunteer.partneraccess')),
       'is_reserved' => 1,
       'label' => 'Volunteer Arlington Partner Access - Volunteer',
       'name' => 'varl_partner_access_volunteer',
